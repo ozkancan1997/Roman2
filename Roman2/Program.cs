d5254a24b16fc1dcc47a2d2aeb7f12873850fdec
ozkancan1997 <97065636+ozkancan1997@users.noreply.github.com>
2022-05-12 20:45:23 +0300

    Add files via upload

---
+++ Roman2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Roman2
+{
+    class Program
+    {
+        static string[] dictionaryX={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        static string[] dictionaryC={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        static string[] dictionaryM={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        static string[] dictionaryMMM={"","M","MM","MMM"};
+
+
+
+        static string[][] dictionary={dictionaryX,dictionaryC,dictionaryM,dictionaryMMM};
+        static void Main(string[] args)
+        {
+           Console.WriteLine(ConvertRoman(2022));
+        }
+        public static string ConvertRoman(int num){
+            int divider=1000;
+            string temp="";
+            for(int i=3;i>=0;i--){
+
+                temp+=dictionary[i][num/divider];
+                num%=divider;
+                divider/=10;
+            }
+            return temp;
+        }
+
+    }
+    
+
+
+}
+
+
+
